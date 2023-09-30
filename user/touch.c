fec415f1842ed13515a7bb9681881092da41273e
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-09-30 13:01:42 +0300

    added feature: you can create a file with content

--- user/touch.c
+++ user/touch.c
@@ -3,8 +3,8 @@
 #include "kernel/fcntl.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: touch [file_name]");
+    if (argc != 2 && argc != 3) {
+        printf("Usage: touch [file_name] [content]");
         exit(1);
     }
     int fd = open(argv[1], O_CREATE | O_RDWR);
@@ -12,5 +12,12 @@ int main(int argc, char *argv[]) {
         printf("Can't create the file\n");
         exit(1);
     }
+    int len = strlen(argv[2]);
+    if (write(fd, argv[2], len) != len) {
+        printf("Can't write to the file\n");
+        close(fd);
+        exit(1);
+    }
+    close(fd);
     exit(0);
 }
